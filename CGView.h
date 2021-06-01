07f7ca1a4bd7302985be4dc20fdb12469e25983c
votran1 <84111295+votran1@users.noreply.github.com>
2021-06-01 14:52:40 +0300

    Update CGView.h

--- CGView.h
+++ CGView.h
@@ -5,7 +5,7 @@
 -(id)initWithFrame;
 -(void)drawRect:(CGRect)rect;
 -(void) addEnemyBox:(float)x y:(float)y w:(float)w h:(float)h;
--(void) addEnemyLine:(float)x y:(float)y;
+-(void) addEnemyLine:(float)x y:(float)y;(999999
 
 -(void) addFriendlyBox:(float)x y:(float)y w:(float)w h:(float)h;
 -(void) addFriendlyLine:(float)x y:(float)y;
